cdce8c69e1730c3247e71e14fde5da5f99c98401
Balint Mucsanyi <mucsanyibalint99@gmail.com>
2025-01-09 19:33:12 +0100

    Fix regularized BMA cross-entropy loss

--- probit/losses/regularized_bma_cross_entropy_loss.py
+++ probit/losses/regularized_bma_cross_entropy_loss.py
@@ -1,7 +1,5 @@
 """Cross-entropy loss combined with Bayesian Model Averaging."""
 
-import torch
-import torch.nn.functional as F
 from torch import nn
 
 from probit.utils.predictive import get_predictive
@@ -12,55 +10,23 @@ from probit.utils.predictive import get_predictive
 class RegularizedBMACrossEntropyLoss(nn.Module):
     """Implements a regularized Cross-entropy loss with Bayesian Model Averaging."""
 
-    def __init__(
-        self, predictive, use_correction, num_mc_samples, regularization_factor
-    ):
+    def __init__(self, regularization_factor):
         super().__init__()
-
-        if not predictive.startswith("log_link"):
-            msg = "Invalid predictive provided"
-            raise ValueError(msg)
 
         self._regularization_factor = regularization_factor
 
-        self.predictive_str = predictive
-
-        if predictive.endswith("mc"):
-            self.loss = nn.NLLLoss()
-        else:
-            self.predictive = get_predictive(
-                predictive, use_correction, num_mc_samples, approximate=False
-            )
-            self.loss = nn.CrossEntropyLoss()
-
-        self.eps = 1e-10
-        self.num_mc_samples = num_mc_samples
+        self.predictive = get_predictive(
+            predictive="sigmoid_mean_field",
+            use_correction=False,
+            num_mc_samples=0,
+            approximate=False,
+        )
+        self.loss = nn.CrossEntropyLoss()
 
     def forward(self, logits, targets):
         mean, var = logits
 
         regularizer = mean.exp().sum(dim=-1).sub(1).square().mean()
-
-        if self.predictive_str.endswith("mc"):
-            logits = (
-                var.sqrt()
-                * torch.randn(
-                    var.shape[0],
-                    self.num_mc_samples,
-                    var.shape[1],
-                    device=var.device,
-                )
-                + mean
-            )
-            logits = logits[0]
-            log_probs = (
-                F.softmax(logits, dim=-1).mean(dim=1).add(self.eps).log()
-            )  # [B, C]
-
-            return (
-                self.loss(log_probs, targets)
-                + self._regularization_factor * regularizer
-            )
 
         logits = self.predictive(mean, var, return_logits=True)
         return self.loss(logits, targets) + self._regularization_factor * regularizer
